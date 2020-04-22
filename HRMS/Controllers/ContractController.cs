879550f0937f5ecacaed8edf2fb55c2671b8cb7c
Dawid Koral <s15153@pjwstk.edu.pl>
2020-04-22 20:44:43 +0200

    Add project files.

---
+++ HRMS/Controllers/ContractController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HRMS.Models;
+
+namespace HRMS.Controllers
+{
+    public class ContractController : Controller
+    {
+
+
+        private s15153Context _context;
+
+        public ContractController(s15153Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contracts()
+        {
+            return View(_context.Contract.ToList());
+        }
+
+    }
+}
